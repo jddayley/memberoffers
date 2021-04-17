e51d0bcb52ad8d0c5c55b755120a8ff4ccceb497
don dayley <jddayley@gmail.com>
2021-04-17 15:35:31 -0400

    Good State.

--- Dev/flaskapp/app/src/app.py
+++ Dev/flaskapp/app/src/app.py
@@ -3,24 +3,12 @@ import os
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 import boto3
+import simplejson
 from boto3.dynamodb.conditions import Key
 
 application = Flask(__name__)
-tableName = "users"
+tableName = "members"
 
-# #application.config["MONGO_URI"] = (
-#     "mongodb://"
-#     + os.environ["MONGODB_USERNAME"]
-#     + ":"
-#     + os.environ["MONGODB_PASSWORD"]
-#     + "@"
-#     + os.environ["MONGODB_HOSTNAME"]
-#     + ":27017/"
-#     + os.environ["MONGODB_DATABASE"]
-#)
-
-# mongo = PyMongo(application)
-# db = mongo.db
 dynamo_client = boto3.client(
     "dynamodb", region_name="us-west-1", endpoint_url="http://dynamodb-local:8000"
 )
@@ -28,72 +16,97 @@ dynamo_client = boto3.client(
 
 @application.route("/")
 def index():
-    return jsonify(
-        status=True, message="DD- Welcome to the Dockerized Flask MongoDB app!"
-    )
+    return jsonify(status=True, message="Welcome to the Dockerized Flask MongoDB app!")
 
 
 @application.route("/inittable")
 def init_table():
     # Get the service resource.
-    # Create the DynamoDB table.
     dynamo_client = boto3.resource(
         "dynamodb", region_name="us-west-1", endpoint_url="http://dynamodb-local:8000"
     )
-
+    # Delete the table before creating it.
     table = dynamo_client.Table(tableName)
-    table.delete()
+    try:
+        table.delete()
+    except:
+        print("error happened")
+     # Create the DynamoDB table.
     table = dynamo_client.create_table(
         TableName=tableName,
         KeySchema=[
-            {"AttributeName": "username", "KeyType": "HASH"},
-            {"AttributeName": "last_name", "KeyType": "RANGE"},
+            {"AttributeName": "PK", "KeyType": "HASH"},
+            {"AttributeName": "SK", "KeyType": "RANGE"},
         ],
         AttributeDefinitions=[
-            {"AttributeName": "username", "AttributeType": "S"},
-            {"AttributeName": "last_name", "AttributeType": "S"},
+            {"AttributeName": "PK", "AttributeType": "S"},
+            {"AttributeName": "SK", "AttributeType": "S"},
+            # {"AttributeName": "memberID", "AttributeType": "N"},
+            # {"AttributeName": "email", "AttributeType": "S"},
+            # {"AttributeName": "createdDate", "AttributeType": "N"},
+            # {"AttributeName": "lastUpdateDate", "AttributeType": "N"},
+            # {"AttributeName": "offerCode", "AttributeType": "S"},
+            # {"AttributeName": "offerID", "AttributeType": "S"},
+            # {"AttributeName": "startDate", "AttributeType": "N"},
+            {"AttributeName": "EndDate", "AttributeType": "N"},
         ],
         ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
+        LocalSecondaryIndexes=[
+            {
+                "IndexName": "OffersByEndDate",
+                "KeySchema": [
+                    {"AttributeName": "PK", "KeyType": "HASH"},  # Partition key
+                    {"AttributeName": "EndDate", "KeyType": "RANGE"},  # Sort key
+                ],
+                "Projection": {"ProjectionType": "ALL"},
+            },
+        ],
     )
+
     # Wait until the table exists.
     table.meta.client.get_waiter("table_exists").wait(TableName=tableName)
     # Print out some data about the table.
     return jsonify(status=True, message="table.item_count")
 
 
-@application.route("/createitem")
-def create_item():
+@application.route("/createmember")
+def create_member():
     # if not dynamodb:
     dynamodb = boto3.resource(
         "dynamodb", region_name="us-west-1", endpoint_url="http://dynamodb-local:8000"
     )
-
     table = dynamodb.Table(tableName)
-    response = table.put_item(Item={"username": "Don", "last_name": "Dayley"})
+    response = table.put_item(
+        Item={
+            "PK": "USER#ddayley",
+            "SK": "#PROFILE#ddayley",
+            "Email": "ddayley@bjs.com",
+            "EndDate": 20210401,
+        }
+    )
     return jsonify(status=True, data=response)
 
 
-# @application.route("/todo")
-# def todo():
-#     _todos = db.todo.find()
-
-#     item = {}
-#     data = []
-#     for todo in _todos:
-#         item = {"id": str(todo["_id"]), "todo": todo["todo"]}
-#         data.append(item)
-
-#     return jsonify(status=True, data=data)
-
-
-# @application.route("/todo", methods=["POST"])
-# def createTodo():
-#     data = request.get_json(force=True)
-#     item = {"todo": data["todo"]}
-#     db.todo.insert_one(item)
-
-#     return jsonify(status=True, message="To-do saved successfully!"), 201
-
+@application.route("/createoffer")
+def create_offer():
+    # if not dynamodb:
+    dynamodb = boto3.resource(
+        "dynamodb", region_name="us-west-1", endpoint_url="http://dynamodb-local:8000"
+    )
+    table = dynamodb.Table(tableName)
+    response = table.put_item(
+        Item={
+            "PK": "USER#ddayley",
+            "SK": "OFFER#bounty",
+            "OfferCode": "21474008",
+            "OfferType": "Recommended",
+            "OfferID": "d517d523-a6d4-4f47-8e5d-c7e7f052bf11",
+            "LastUpdatedDate": "04-01-21",
+            "StartDate": "2021-04-01",
+            "EndDate": 20210401,
+        }
+    )
+    return jsonify(status=True, data=response)
 
 @application.route("/items")
 def get_items():
@@ -101,17 +114,17 @@ def get_items():
         "dynamodb", region_name="us-west-1", endpoint_url="http://dynamodb-local:8000"
     )
     table = dynamodb.Table(tableName)
-    response = table.scan()
-    # data = response['Items']
-
-    # while 'LastEvaluatedKey' in response:
-    #    response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-    #    data.extend(response['Items'])
-    # response = table.query(
-    #     KeyConditionExpression=Key('username').eq('HASH')
-    # )
-    # list = [(k, v) for k, v in response.items()]
-    return jsonify(status=True, message=response), 201
+    cDate = "2021-04-01"
+    memberID = "USER#ddayley"
+    response = table.query(
+        TableName="users",
+        IndexName="OffersByEndDate",
+        KeyConditionExpression=Key("PK").eq("USER#ddayley")
+        & Key("EndDate").eq(20210401),
+    )
+    #print(response)
+    return simplejson.dumps(response['Items']), 201
+    # jsonify(status=True, message=response), 201
 
 
 if __name__ == "__main__":
